f871707a25422b650455e9a10909f84da6537a43
Sounak <sounak.das@vitra.ai>
2025-03-04 08:26:08 +0530

    talk.io added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,7 @@ import "/styles/css/card.scss";
 import "/styles/css/globals.scss";
 import { GoogleTagManager } from "@next/third-parties/google";
 import NavSection from "./components/NavSection";
-import { useEffect } from "react";
-
+import TawkTo from "./components/TawkTo";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,22 +18,10 @@ const RootLayout = ({
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://embed.tawk.to/67c6695cdbaee8190d001eed/1ilfgn4rh";
-    script.async = true;
-    script.charset = "UTF-8";
-    script.setAttribute("crossorigin", "*");
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
   return (
     <html lang="en">
       <body className={inter.className}>
+        <TawkTo />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
           <NavSection />
           {children}
@@ -46,22 +33,3 @@ const RootLayout = ({
 };
 
 export default RootLayout;
-
-
-
-
-/*
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/67c6695cdbaee8190d001eed/1ilfgn4rh';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
-*/
