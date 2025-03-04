37dd816687973895ae5c8fd30b4c16154c58affa
Sounak <60914738+LittleBoy9@users.noreply.github.com>
2025-03-04 08:26:08 +0530

    talk.io added

---
+++ app/components/TawkTo.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useEffect } from "react";
+
+const TawkTo = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://embed.tawk.to/67c6695cdbaee8190d001eed/1ilfgn4rh";
+    script.async = true;
+    script.charset = "UTF-8";
+    script.setAttribute("crossorigin", "*");
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return null; // This component does not render anything
+};
+
+export default TawkTo;
+
+/*
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/67c6695cdbaee8190d001eed/1ilfgn4rh';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+*/
