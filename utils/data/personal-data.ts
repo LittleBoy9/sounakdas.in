27cd8ab47c1b8d75827951b6180d2f41443eeaf6
Sounak <sounak.das@vitra.ai>
2025-03-04 08:55:22 +0530

    leetcode added

--- utils/data/personal-data.ts
+++ utils/data/personal-data.ts
@@ -1,7 +1,7 @@
 export const personalData = {
     name: "SOUNAK DAS",
     profile: '/png/profile.png',
-    designation: "Software Developer",
+    designation: "Senior Software Engineer",
     description: "My name is SOUNAK DAS. I am a dedicated and results-driven Senior Software Engineer with a passion for building scalable and efficient solutions. I have a strong problem-solving mindset and a deep enthusiasm for learning and adapting to new technologies. With a keen focus on AI-powered applications, I strive to create seamless and intelligent systems that enhance user experiences. My expertise lies in JavaScript and its modern frameworks, and I enjoy architecting robust applications that push the boundaries of innovation. I am always open to opportunities that align with my skills and allow me to contribute to impactful projects.",
     email: 'iamsounak01@gmail.com',
     phone: '+91 7384449005',
@@ -11,7 +11,7 @@ export const personalData = {
     linkedIn: 'https://www.linkedin.com/in/sounakdas/',
     twitter: '',
     stackOverflow: 'https://stackoverflow.com/users/11578976/sounak-das',
-    leetcode: "",
+    leetcode: "https://leetcode.com/u/sounak07/",
     devUsername: "",
     resume: "https://drive.google.com/file/d/1jz_54E8WhYBQLymo1O3vO9GwbQCqGoet/view?usp=sharing"
   }
